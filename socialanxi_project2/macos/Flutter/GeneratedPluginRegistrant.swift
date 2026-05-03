f534b2cf55812a5190abcfe51b58be7baa04d2a9
Justin Wu <justinwu12138@gmail.com>
2026-05-03 16:26:11 -0400

    Add core dependencies

--- socialanxi_project2/macos/Flutter/GeneratedPluginRegistrant.swift
+++ socialanxi_project2/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,26 @@
 import FlutterMacOS
 import Foundation
 
+import audio_service
+import audio_session
+import cloud_firestore
+import firebase_auth
+import firebase_core
+import firebase_messaging
+import firebase_storage
+import google_sign_in_ios
+import just_audio
+import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioServicePlugin.register(with: registry.registrar(forPlugin: "AudioServicePlugin"))
+  AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
+  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+  FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
+  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
+  JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
